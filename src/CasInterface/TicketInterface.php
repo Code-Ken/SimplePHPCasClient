c58e84d6b2e9dde94a9f4a5c02b237978e088f32
Ken <gaosong0301@foxmail.com>
2018-03-20 15:46:39 +0800

    +)add interface

---
+++ src/CasInterface/TicketInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SimplePHPCasClient\CasInterface;
+
+/**
+ * Interface TicketInterface
+ * @package SimplePHPCasClient\CasInterface
+ */
+interface TicketInterface
+{
+    /**
+     * @param string $ticket
+     * @return bool
+     */
+    public function invalidTicket(string $ticket): bool;
+
+    /**
+     * @param string $ticket
+     * @return bool
+     */
+    public function saveTicket(string $ticket): bool;
+
+    /**
+     * @param string $ticket
+     * @return bool
+     */
+    public function checkTicket(string $ticket): bool;
+}
